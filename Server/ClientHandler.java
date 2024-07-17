14895dcf5b0d7f1ecf038b2573bde67a8c9296b9
praiseGit34 <praiseasiimire38@gmail.com>
2024-07-17 09:38:59 +0300

    created a client handler class that will handle all the functionalities in the server

---
+++ Server/ClientHandler.java
@@ -0,0 +1,17 @@
+package Server;
+
+import java.net.Socket;
+import java.sql.Connection;
+
+public class ClientHandler extends Thread {
+
+    public ClientHandler(Socket accept, Connection dbConnection) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+}
