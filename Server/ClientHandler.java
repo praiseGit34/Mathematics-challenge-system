e25ccf9d3be0f11133f56e1f08ef718be4f91ea0
praiseGit34 <162679130+praiseGit34@users.noreply.github.com>
2024-07-22 13:11:05 +0300

    Update ClientHandler.java

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -162,7 +162,7 @@ public class ClientHandler implements Runnable {
                 message.setFrom(new InternetAddress(from));
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAddress));
                 message.setSubject("subject");
-                message.setText(password);
+                message.setText(body);
         
                 Transport.send(message);
                 System.out.println("Email sent successfully to " + emailAddress);
